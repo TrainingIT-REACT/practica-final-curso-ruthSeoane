cd924341faaa03d07039583c617d6fc5d329810d
Ruth Seoane Loureda <ruth.seoane@altia.es>
2020-02-12 16:39:36 +0100

    Colores de la barra de navegacion ok cuando clicas en el dropdown

--- src/App/navBar/NavBar.js
+++ src/App/navBar/NavBar.js
@@ -24,7 +24,7 @@ class NavBar extends Component{
       super(props);
       this.updateUser = this.updateUser.bind(this);
 
-      this.state = {
+       this.state = {
         signedIn: false,
         updateUser: this.updateUser,
       }
@@ -43,12 +43,23 @@ class NavBar extends Component{
                 <div className="navBar">
                   <NavLink exact activeClassName="active" to="/" className="elemento">Inicio</NavLink>
                   <NavLink activeClassName="active" to="/mi_musica" className="elemento">Mi m&uacute;sica</NavLink>
-                  <NavLink activeClassName="active" to="/perfil" className="elemento perfil">
-                  <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-                    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z"/>
-                  </svg>                    
-                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Mi perfil
-                  </NavLink>
+
+                    <div className="dropdown elemento perfil" id = "botonOrigen">
+                      <div>
+                      <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+                      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z"/>
+                    </svg>                    
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Mi perfil
+
+
+                  {/* Le añado al elemento un onClick y creo una funcion que pille un elemento por id y le ponga la clase activa */}
+
+                    </div>
+                      <div className="dropdown-content">
+                        <NavLink to="/perfil">Ver perfil</NavLink>
+                        {/* <NavLink to="/perfil">Login</NavLink> */}
+                      </div>
+                    </div> 
                 </div>
                 <Route path="/" exact component={Home}/>
                 <PrivateRoute path="/mi_musica" component={MiMusica}/>
--- src/App/rutas/login/Login.js
+++ src/App/rutas/login/Login.js
@@ -6,6 +6,16 @@ import { Route, Redirect } from 'react-router-dom';
 import UserContext from "../../privateRoute/UserContext";
 
 class Login extends React.Component{
+
+    componentDidMount() {
+        document.getElementById("botonOrigen").classList.add("active")
+      }
+
+      componentWillUnmount(){
+        document.getElementById("botonOrigen").classList.remove("active")   
+      }
+
+
     render(){
         return <UserContext.Consumer>
             {({ signedIn, updateUser }) => {
